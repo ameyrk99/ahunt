080a50401e821c5b287b3ccc3129d4a74c6ae2dc
Amey Khairnar <ameykhairnar99@gmail.com>
2018-10-06 14:19:04 -0500

    css-loader missing

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 const app = document.getElementById('root')
 ReactDOM.render(
    <div>
        Olivier is Awesome
+
+       <Button type="primary">Button</Button>
    </div>
 
     , app);
