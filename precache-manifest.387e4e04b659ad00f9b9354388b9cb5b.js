b541fa88bfb8c0df843bee6bbcbaf69f2a92b34c
Amey Khairnar <ameykhairnar99@gmail.com>
2018-10-09 23:15:24 -0500

    Updates

---
+++ precache-manifest.387e4e04b659ad00f9b9354388b9cb5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b553f1dac92e9c6f677",
+    "url": "/ahunt/static/js/runtime~main.9b553f1d.js"
+  },
+  {
+    "revision": "a99651020bf65137dce8",
+    "url": "/ahunt/static/js/main.a9965102.chunk.js"
+  },
+  {
+    "revision": "7e28e11e68df8c9db248",
+    "url": "/ahunt/static/js/1.7e28e11e.chunk.js"
+  },
+  {
+    "revision": "a99651020bf65137dce8",
+    "url": "/ahunt/static/css/main.62bc8234.chunk.css"
+  },
+  {
+    "revision": "487f60e679ab6f758d540531523ae9a1",
+    "url": "/ahunt/index.html"
+  }
+];
